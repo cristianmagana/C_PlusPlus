6c9efb0b45a5082674fccb78ed974e89afef43aa
Cristian Magana <crmagan1@uci.edu>
2020-08-06 09:44:57 -0700

    First Commit

---
+++ Lab1/Functions.h
@@ -0,0 +1,18 @@
+/*
+ * Functions.h
+ *
+ *  Created on: August 5th, 2020
+ *      Author: Cristian Magana
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <string>
+using namespace std;
+
+string printPyramid(int &num);
+int getUserInput();
+std::string rightSideBox(std::string box, int &num);
+
+#endif /* FUNCTIONS_H_ */
---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Lab1.cpp
+// Author      : Cristian Magana
+// Version     : 1
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "Functions.h"
+#include <iostream>
+
+int main() {
+
+	int pyramidSize = getUserInput();
+
+	printPyramid(pyramidSize);
+
+	return 0;
+}
