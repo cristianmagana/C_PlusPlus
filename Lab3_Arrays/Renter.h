df9d82884cef6fc8131b82801a857381499d0b73
engineeredkaos <33166233+engineeredkaos@users.noreply.github.com>
2020-08-24 11:20:36 -0700

    Create Renter.h

---
+++ Lab3_Arrays/Renter.h
@@ -0,0 +1,26 @@
+#ifndef RENTER_H_
+#define RENTER_H_
+
+#include "Exceptions.cpp"
+
+class Renter {
+
+private:
+	int renterID;
+	std::string renterFirstName;
+	std::string renterLastName;
+
+public:
+	Renter();
+	Renter(int rID, std::string rFirstName, std::string rLastName);
+	void setRenterID(int rID);
+	int getRenterID();
+	void setRenterFirstName(std::string rFirstName);
+	std::string getRenterFirstName();
+	void setRenterLastName(std::string rLastName);
+	std::string getRenterLastName();
+
+	friend ostream& operator<<(ostream& os, Renter rentee);
+};
+
+#endif
