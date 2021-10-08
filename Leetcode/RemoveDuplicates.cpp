e63b61848fe2a0f932d75d0538c67f68281375d9
digitalxtian <33166233+digitalxtian@users.noreply.github.com>
2021-10-07 17:16:19 -0700

    Create RemoveDuplicates.cpp

---
+++ Leetcode/RemoveDuplicates.cpp
@@ -0,0 +1,14 @@
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {       
+        for(int i=1; i<(nums.size()); ++i)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                nums.erase(nums.begin()+i);
+                i = i-1;
+            }
+        }        
+        return nums.size();
+    }
+};
